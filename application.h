e64b9d2532be32422af3864f21daff31b30accda
Enakaev Aleksey <enakaev.aleksey@yandex.ru>
2024-09-17 11:35:01 +0300

    Application: Добавлен класс QThreadDebug, логгирующий создание/удаление

--- application.h
+++ application.h
@@ -1,11 +1,25 @@
 #pragma once
 
 #include <QMainWindow>
+#include <QThread>
+#include <QDebug>
 
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Application; }
 QT_END_NAMESPACE
+
+
+class QThreadDebug : public QThread {
+     Q_OBJECT
+public:
+     QThreadDebug() : QThread{} {
+          qInfo() << "Thread (debug) created";
+     }
+     ~QThreadDebug() {
+          qInfo() << "Thread (debug) destroyed";
+     }
+};
 
 
 class Application : public QMainWindow
